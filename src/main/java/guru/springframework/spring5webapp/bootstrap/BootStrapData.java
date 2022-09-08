7971c5234cf2f35a7349fcefc1bbf283042647eb
Harry S. Green <harrysgreen@Harrys-MBP.fios-router.home>
2022-09-08 16:21:36 -0400

    Updates for adding Publisher

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,57 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import guru.springframework.spring5webapp.repositories.*;
+import guru.springframework.spring5webapp.model.*;
+
+@Component
+public class BootStrapData implements CommandLineRunner
+{
+	private final AuthorRepository authorRepository;
+	private final BookRepository bookRepository;
+	private final PublisherRepository publisherRepository;
+	
+	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository)
+	{
+		super();
+		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
+	}
+
+	@Override
+	public void run(String... args) throws Exception
+	{
+		// TODO Auto-generated method stub
+		Author eric = new Author("Eric", "Evans");
+		Book ddd = new Book("Domain Driven Design","123123");
+		eric.getBooks().add(ddd);
+		ddd.getAuthors().add(eric);
+		authorRepository.save(eric);
+		bookRepository.save(ddd);
+		
+		Author rod = new Author("Rod", "Johnson");
+		Book noEJB = new Book("J2EE Development Without EJB", "3939459459");
+		rod.getBooks().add(noEJB);
+		noEJB.getAuthors().add(rod);
+		authorRepository.save(rod);
+		bookRepository.save(noEJB);
+		
+		Publisher addisonWesley = new Publisher("Addison-Wesley","501 Boylston St","Boston","MA","02116");
+		publisherRepository.save(addisonWesley);
+		
+		Publisher wiley = new Publisher("Wiley","111 River Street Hoboken","Hoboken","NJ","07030");
+		publisherRepository.save(wiley);
+		
+		System.out.println("Started in Bootstrap");
+		System.out.println("Number of Books: " + bookRepository.count());
+		System.out.println("Number of Authors: " + authorRepository.count());
+		System.out.println("Number of Publishers: " + publisherRepository.count());
+	}
+
+}
---
+++ src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -0,0 +1,120 @@
+package guru.springframework.spring5webapp.model;
+
+import java.util.Objects;
+
+//import javax.persistence.Entity;
+//import javax.persistence.GenerationType;
+import javax.persistence.*;
+
+@Entity
+public class Publisher
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	private String addressLine1;
+	private String city;
+	private String state;
+	private String zip;
+
+	public Publisher() {		
+	}
+
+	public Publisher(String name, String addressLine1, String city, String state, String zip) {
+		this.name = name;
+		this.addressLine1 = addressLine1;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getAddressLine1()
+	{
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1)
+	{
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getCity()
+	{
+		return city;
+	}
+
+	public void setCity(String city)
+	{
+		this.city = city;
+	}
+
+	public String getState()
+	{
+		return state;
+	}
+
+	public void setState(String state)
+	{
+		this.state = state;
+	}
+
+	public String getZip()
+	{
+		return zip;
+	}
+
+	public void setZip(String zip)
+	{
+		this.zip = zip;
+	}
+
+	@Override
+	public String toString()
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append("Publisher [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", addressLine1=");
+		builder.append(addressLine1);
+		builder.append(", city=");
+		builder.append(city);
+		builder.append(", state=");
+		builder.append(state);
+		builder.append(", zip=");
+		builder.append(zip);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return id != null ? id.hashCode() : 0;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj) return true;
+		if (obj == null || getClass() != obj.getClass()) return false;
+		
+		Publisher publisher = (Publisher)obj;
+		
+		return id != null ? id.equals(publisher.id) : publisher.id == null;
+	}
+
+}
