83ddb1ff02dc103d1b218281306af9930a6a42ac
Harry S. Green <harrysgreen@Harrys-MBP.fios-router.home>
2022-09-14 10:07:19 -0400

    Corrected with Thymeleaf to display

---
+++ src/main/java/guru/springframework/spring5webapp/config/MvcWebConfig.java
@@ -0,0 +1,46 @@
+package guru.springframework.spring5webapp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("guru.springframework.spring5webapp")
+public class MvcWebConfig implements WebMvcConfigurer {
+
+	@Autowired
+	ApplicationContext applicationContext;
+	
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setApplicationContext(applicationContext);
+		templateResolver.setPrefix("classpath:/template/");
+		templateResolver.setSuffix(".html");
+		return templateResolver;
+	}
+	
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		templateEngine.setEnableSpringELCompiler(true);
+		return templateEngine;
+	}
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		registry.viewResolver(resolver);
+	}
+}
--- src/main/java/guru/springframework/spring5webapp/controller/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controller/BookController.java
@@ -1,25 +1,23 @@
 package guru.springframework.spring5webapp.controller;
 
 import guru.springframework.spring5webapp.repositories.BookRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BookController
 {
-	private final BookRepository bookRepository;
+	@Autowired
+	BookRepository bookRepository;
 	
-	public BookController(BookRepository bookRepository)
-	{
-		super();
-		this.bookRepository = bookRepository;
-	}
-
-	@RequestMapping("/books")
+	@RequestMapping(value = "/books", method=RequestMethod.GET)
 	public String getBooks(Model model)
 	{
 		model.addAttribute("books", bookRepository.findAll());
-		return "books";
+		return "books/list";
 	}
 }
