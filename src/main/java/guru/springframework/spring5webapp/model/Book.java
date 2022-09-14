13c442856c5d7eef319b1f9c723285110902e7fc
Harry S. Green <harrysgreen@Harrys-MBP.fios-router.home>
2022-09-14 16:07:51 -0400

    Removed circular dependencies from Author, Book

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -106,7 +106,6 @@ public class Book {
 	@Override
 	public String toString()
 	{
-		return "Book [id=" + id + ", title=" + title + ", isbn=" + isbn + ", publisher=" + publisher + ", authors="
-				+ authors + "]";
+		return "Book [id=" + id + ", title=" + title + ", isbn=" + isbn + "]";
 	}
 }
